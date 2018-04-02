9fff275c6a41da9178d39233635b2f635777c668
Martin Boyakhchyan <martin@bmby.com>
2018-04-02 16:05:24 +0300

    propertyqueryparams

--- lib/querystrings/index.ts
+++ lib/querystrings/index.ts
@@ -2,3 +2,4 @@ export * from './CityQueryParams';
 export * from './NeighbourhoodQueryParams';
 export * from './RegionQueryParams';
 export * from './StreetQueryParams';
+export * from './PropertyQueryParams';
